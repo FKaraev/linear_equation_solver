ce816dbe70d8b471b81b335d854eca5f569237f3
Farrukh <45211043+FKaraev@users.noreply.github.com>
2020-08-06 12:20:45 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -37,31 +37,5 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
-
-
-    static  void demo(){
-
-        ComplexNumber [][] A;
-        ComplexNumber number1 = new ComplexNumber(1,1);
-        ComplexNumber number2 = ComplexNumber.ONE;
-        ComplexNumber number3 = new ComplexNumber(3,0);
-        ComplexNumber number5 = ComplexNumber.ONE;
-        ComplexNumber number4 = ComplexNumber.ZERO;
-        ComplexNumber number6 = new ComplexNumber(4,0);
-
-        A = new ComplexNumber[][]{{number1,number2,number3},{number4,number5,number6}};
-
-        Matrix matrix = new Matrix(A);
-
-        LinearEquationSolver solver = new LinearEquationSolver(matrix);
-        solver.solve();
-        
-        System.out.println(matrix);
-
-
-    }
-
 }
